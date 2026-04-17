54e153c395ad7ab5f7c80ccdb9b0935f6cedee08
-v <agilecoderfrank@gmail.com>
2026-04-17 16:38:20 +0200

    lint

--- src/TypedJson.test.ts
+++ src/TypedJson.test.ts
@@ -1,36 +1,42 @@
-import { describe, expect, test } from 'vitest';
-import { TypedJson } from './TypedJson.js';
-import { readFileSync } from "node:fs"
+import { describe, expect, test } from "vitest";
+import { TypedJson } from "./TypedJson.js";
+import { readFileSync } from "node:fs";
 
-describe('TypedJson', async () => {
-  const wasmBuffer = readFileSync('./src//wasm/typedJson.wasm');
-  const typedJson = await TypedJson.load(wasmBuffer.buffer)
+describe("TypedJson", async () => {
+  const wasmBuffer = readFileSync("./src/wasm/typedJson.wasm");
+  const typedJson = await TypedJson.load(wasmBuffer.buffer);
 
-  test('version', async () => {
-    typedJson.version()
+  test("version", async () => {
+    typedJson.version();
     expect(typedJson.version()).toEqual("0.9.1");
   });
 
-  test('validate', async () => {
-    const basicOutput = await typedJson.validate(`{"type":"string"}`, "13")
+  test("validate", async () => {
+    const basicOutput = await typedJson.validate(`{"type":"string"}`, "13");
     expect(basicOutput).toEqual({
-      valid: false, errors: [{
-        error: "expected type: string",
-        instanceLocation: "",
-        keywordLocation: "/type",
-      }]
-    })
-  })
+      valid: false,
+      errors: [
+        {
+          error: "expected type: string",
+          instanceLocation: "",
+          keywordLocation: "/type",
+        },
+      ],
+    });
+  });
 
-  test('suggest', async () => {
-    const suggestOutput = await typedJson.suggest(`{"type":"string"}`, "13", "", false)
+  test("suggest", async () => {
+    const suggestOutput = await typedJson.suggest(
+      `{"type":"string"}`,
+      "13",
+      "",
+      false,
+    );
     expect(suggestOutput).toEqual([
       {
         location: "/type",
-        values: [
-          "",
-        ],
+        values: [""],
       },
-    ])
-  })
+    ]);
+  });
 });
